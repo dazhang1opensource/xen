7eb029328583409494fff11b5bf1eb1e63c49aee
Haozhong Zhang <haozhong.zhang@intel.com>
2017-08-14 22:34:59 +0800

    xen/pmem: support PMEM_REGION_TYPE_MGMT for XEN_SYSCTL_nvdimm_pmem_get_regions
    Allow XEN_SYSCTL_nvdimm_pmem_get_regions to return a list of
    management PMEM regions.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>

--- tools/libxc/xc_misc.c
+++ tools/libxc/xc_misc.c
@@ -939,6 +939,10 @@ int xc_nvdimm_pmem_get_regions(xc_interface *xch, uint8_t type,
         size = sizeof(xen_sysctl_nvdimm_pmem_raw_region_t) * max;
         break;
 
+    case PMEM_REGION_TYPE_MGMT:
+        size = sizeof(xen_sysctl_nvdimm_pmem_mgmt_region_t) * max;
+        break;
+
     default:
         return -EINVAL;
     }
@@ -958,6 +962,10 @@ int xc_nvdimm_pmem_get_regions(xc_interface *xch, uint8_t type,
     {
     case PMEM_REGION_TYPE_RAW:
         set_xen_guest_handle(regions->u_buffer.raw_regions, buffer);
+        break;
+
+    case PMEM_REGION_TYPE_MGMT:
+        set_xen_guest_handle(regions->u_buffer.mgmt_regions, buffer);
         break;
 
     default:
