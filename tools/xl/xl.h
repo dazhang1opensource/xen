6d30b08157fe48113d0b42bd8af754624eb1be8a
Haozhong Zhang <haozhong.zhang@intel.com>
2017-10-26 16:46:53 +0800

    tools/xl: add xl command 'pmem-list'
    The new xl command 'pmem-list' is used to list the information of PMEM
    regions.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>

--- tools/xl/xl.h
+++ tools/xl/xl.h
@@ -212,6 +212,7 @@ int main_psr_mba_set(int argc, char **argv);
 int main_psr_mba_show(int argc, char **argv);
 #endif
 int main_qemu_monitor_command(int argc, char **argv);
+int main_pmem_list(int argc, char **argv);
 
 void help(const char *command);
 
